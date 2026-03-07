d20e1d0b3f146d725fbfb53a8201f951211c768d
AsRealTech <114587693+AsRealTech@users.noreply.github.com>
2026-03-07 06:42:40 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,22 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
+let replitPlugins = [];
+
+if (process.env.NODE_ENV !== "production" && process.env.REPL_ID !== undefined) {
+  const cartographer = (await import("@replit/vite-plugin-cartographer")).cartographer;
+  const devBanner = (await import("@replit/vite-plugin-dev-banner")).devBanner;
+
+  replitPlugins = [cartographer(), devBanner()];
+}
+
 export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-          await import("@replit/vite-plugin-dev-banner").then((m) =>
-            m.devBanner(),
-          ),
-        ]
-      : []),
+    ...replitPlugins,
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
@@ -26,11 +26,14 @@ export default defineConfig({
       "@assets": path.resolve(import.meta.dirname, "attached_assets"),
     },
   },
+
   root: path.resolve(import.meta.dirname, "client"),
+
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
   },
+
   server: {
     fs: {
       strict: true,
